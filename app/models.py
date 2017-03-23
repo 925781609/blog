fb24728d51dfafb379397dd282fa79d913c4a050
925781609 <925781609@qq.com>
2017-03-23 14:31:09 +0800

    add blog relevant models

--- app/models.py
+++ app/models.py
@@ -61,4 +61,32 @@ class User(AbstractBaseUser):
     def is_staff(self):
         ''' Is the user a member of staff? '''
         return self.is_admin
-    
+   """   Blog  relevant model """
+
+class Blog (models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Author( models.Model ):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Entry( models.Model ):
+    blog = models.ForeignKey( Blog )
+    headline = models.CharField( max_length=255 )
+    body__text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField( Author )
+    n_comments = models.IntegerField()
+    n_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.headline
